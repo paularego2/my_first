71777d42bf91140f05ddd75a74ffdc8de7eeb14b
paula <paularego.estg@gmail.com>
2019-03-01 16:43:36 +0000

    add weather API

--- index.js
+++ index.js
@@ -1,3 +1,161 @@
-$(document.ready(function(){
-    alert("ok)");
-}))
+var object;
+$(document).ready(function(){
+    object=new Object();
+});
+var Object = function (){
+    
+    var that = this;
+    this.API_KEY = '0055da9ada92f19b7815b5e205c84477';
+
+    //Faz pedido API
+    this.readName = function(cidade) {
+    //Chamada api para nome da cidade
+    $.get("http://api.openweathermap.org/data/2.5/weather?q="
+    + cidade + "&appid=" + that.API_KEY, function(res) {
+        that.render(res);
+        });
+        };
+    
+     this.render=function(res){
+        $("[data-name-cidade]").html(res.name);
+      //Adicionar o termo geral do tempo
+    $("[data-main-cidade]").html(res.weather[0]['main']);
+    //Adicionar a descrição do tempo
+    $("[data-description-cidade]").html(res.weather[0]['description'])
+;
+//Adicionar a temperatura minima
+$("[data-temp-min-cidade]").html(Math.round(res.main.temp_min -273.15) + 'º');
+    //Adicionar a temperatura maxima
+    $("[data-temp-max-cidade]").html(Math.round(res.main.temp_max -273.15) + 'º');
+        //Adicionar a humidade
+        $("[data-humidity-cidade]").html(res.main.humidity + '%');
+        //Adicionar a velocidade do vento
+        $("[data-wind-cidade]").html(res.wind.speed + ' m/s');
+        //Adicionar a precipitação
+        $("[data-pressure-cidade]").html(res.main.pressure + 'hpa');
+        //Adicionar a temperatura
+        $("[data-temp-cidade]").html(Math.round(res.main.temp -273.15) + 'º');
+            //Colocar o icon do tempo certo
+            switch (res.weather[0]['icon']) {
+            case '01n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/clear-night.png');
+            break;
+            case '01d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/clear-day.png');
+            break;
+            case '02n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/partly-cloudy-night.png');
+            break;
+
+            case '02d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/partly-cloudy-day.png');
+            break;
+            case '01n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/clear-night.png');
+            break;
+            case '03n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/cloudy.png');
+            break;
+            case '03d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/cloudy.png');
+            break;
+            case '04n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/cloudy.png');
+            break;
+            case '04d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/cloudy.png');
+            break;
+            case '04n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/cloudy.png');
+            break;
+            case '04d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/cloudy.png');
+            break;
+            case '09n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/rain.png');
+            break;
+            case '09d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/rain.png');
+            break;
+            case '10n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/rain.png');
+            break;
+            case '10d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/rain.png');
+            break;
+            case '11n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/thunderstorm.png');
+            break;
+            case '11d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/thunderstorm.png');
+            break;
+            case '13n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/snow.png');
+            break;
+            case '13d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/snow.png');
+            break;
+            case '50n':
+            $("[data-img-weather]").attr('src',
+            'img/icons/fog.png');
+            break;
+            case '50d':
+            $("[data-img-weather]").attr('src',
+            'img/icons/fog.png');
+            break;
+            default:
+            break;
+            }
+            };
+
+
+
+     }
+    this.setup = function(){
+        //criação de eventos};
+
+        $("[data-input-cidade]").on('keypress', function(e) {    
+            if (e.which == 13) {
+                 var cidade = $("[data-input-cidade]").val(); 
+                 
+                // console.log(cidade);
+                that.readName(cidade);          
+                 }   
+            });
+
+         //Evento para quando se carregar no btn ele pesquisar a cidade    
+         $("[data-btn-cidade]").on('click', function() { 
+            var cidade = $("[data-input-cidade]").val(); 
+            //console.log(cidade); 
+             that.readName(cidade);  
+             });
+
+    }
+    this.start=function()
+    {
+        that.readNome();
+        that.setup();
+    }
+
+    that.start();
+    
+}
